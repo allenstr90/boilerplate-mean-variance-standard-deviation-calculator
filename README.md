6a42935cf02c8fbbd817f8bfbc2984a00f6e3887
allenstr90 <allenstr90@gmail.com>
2021-07-25 23:53:59 -0400

    fix: completed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### Completed !!!
+
 ### Assignment
 
 Create a function named `calculate()` in `mean_var_std.py` that uses Numpy to output the mean, variance, standard deviation, max, min, and sum of the rows, columns, and elements in a 3 x 3 matrix. 
--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,23 @@
+from builtins import len
+
 import numpy as np
 
+
 def calculate(list):
+    if len(list) < 9:
+        raise ValueError('List must contain nine numbers.')
 
+    arr = np.array(list)
+    mtx = arr.reshape(3, 3)
 
-
-
+    calculations = {
+        'mean': [np.mean(mtx, axis=0).tolist(), np.mean(mtx, axis=1).tolist(), np.mean(arr)],
+        'variance': [np.var(mtx, axis=0).tolist(), np.var(mtx, axis=1).tolist(), np.var(arr)],
+        'standard deviation': [np.std(mtx, axis=0).tolist(), np.std(mtx, axis=1).tolist(), np.std(arr)],
+        'max': [np.max(mtx, axis=0).tolist(), np.max(mtx, axis=1).tolist(), np.max(arr)],
+        'min': [np.min(mtx, axis=0).tolist(), np.min(mtx, axis=1).tolist(), np.min(arr)],
+        'sum': [np.sum(mtx, axis=0).tolist(), np.sum(mtx, axis=1).tolist(), np.sum(arr)]}
     return calculations
+
+# if __name__ == '__main__':
+#     print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
